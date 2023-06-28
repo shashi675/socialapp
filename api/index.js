9e91ab945dde970780c65a13280e092f1398c7e3
shashi675 <shashikantrai675@gmail.com>
2023-06-29 01:18:12 +0530

    com

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 })
-app.use(cors({origin:'http://localhost:3000'}));
+app.use(cors({origin:'https://sksocialapp.netlify.app'}));
 
 
 const storage = multer.diskStorage({
