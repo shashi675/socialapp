7e43a4a9a173355899217f5ef55da5bdb5670394
shashi675 <shashikantrai675@gmail.com>
2023-06-29 04:10:57 +0530

    crroo

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     next();
 })
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin: 'https://sksocialapp.netlify.app',
     optionsSuccessStatus: 200,
     withCredentials: true
 }));
