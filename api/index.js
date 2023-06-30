359600376267066372646b3b0b66424ae0ad0505
shashi675 <shashikantrai675@gmail.com>
2023-06-30 23:59:05 +0530

    cors errr

--- api/index.js
+++ api/index.js
@@ -21,9 +21,9 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());    
 app.use((req, res, next) => {
-        res.set("Access-Control-Allow-Credentials", true);
-        res.set("Access-Control-Allow-Origin", "https://sksocialapp.netlify.app");
-        res.set("Access-Control-Allow-Headers", "content-type");
+        res.setHeader("Access-Control-Allow-Credentials", true);
+        res.appendHeader("Access-Control-Allow-Origin", "https://sksocialapp.netlify.app");
+        res.appendHeader("Access-Control-Allow-Headers", "content-type");
         next();
     })
     // app.use(cors({
