79f73240d36c8dad195df356a58d088495446632
shashi675 <shashikantrai675@gmail.com>
2023-06-30 22:47:37 +0530

    cors er

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());    
 app.use((req, res, next) => {
         res.header("Access-Control-Allow-Credentials", true);
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+        res.header("Access-Control-Allow-Origin", "https://sksocialapp.netlify.app/");
         next();
     })
     app.use(cors({
